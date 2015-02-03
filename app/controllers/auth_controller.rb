a0b34ceea7a4ee215ea7b97ff5e67ce21d237d11
James Smith <james@floppy.org.uk>
2015-02-03 16:03:57 +0000

    store github auth token

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,6 +3,7 @@ class AuthController < ApplicationController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      session[:github_token] = request.env["omniauth.auth"]['credentials']['token']
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       flash[:notice] = "Logged in" if is_navigational_format?
     else
