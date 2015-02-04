dfd4828b699b88d974a7a5d683ddcc88d346c5b0
James Smith <james@floppy.org.uk>
2015-02-04 18:37:33 +0000

    Add username and avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.username = auth.info.nickname
+      user.avatar_url = auth.info.image
     end
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203132753) do
+ActiveRecord::Schema.define(version: 20150204173914) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20150203132753) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "username"
+    t.string   "avatar_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
